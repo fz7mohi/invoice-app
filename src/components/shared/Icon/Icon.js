b65164e5f6269efec6f551db00e46ab699cdf96f
Fayas <fayas47@gmail.com>
2025-04-04 11:13:31 +0300

    Update package.json: Modify build script to use '--legacy-peer-deps' for npm install and remove react-icomoon dependency, transitioning to react-icons for icon management in Icon component.

--- src/components/shared/Icon/Icon.js
+++ src/components/shared/Icon/Icon.js
@@ -1,15 +1,20 @@
-import IcoMoon from 'react-icomoon';
-import iconSet from './selection.json';
+import { IconBaseProps } from 'react-icons';
+import * as Icons from 'react-icons/all';
 
-const Icon = ({ name, size, color, customStyle }) => {
+const Icon = ({ name, size, color, customStyle, ...props }) => {
+    const IconComponent = Icons[name];
+    
+    if (!IconComponent) {
+        console.warn(`Icon "${name}" not found`);
+        return null;
+    }
+
     return (
-        <IcoMoon
-            iconSet={iconSet}
-            disableFill="true"
-            icon={name}
+        <IconComponent
             size={size}
             color={color}
             style={customStyle}
+            {...props}
         />
     );
 };
